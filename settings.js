28a79771740f88d79b104e68511d5453806c686c
MKwapisz <164620906+MKwapisz2000@users.noreply.github.com>
2025-10-05 20:10:51 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+adminAuth: {
+    type: "credentials",
+    users: [{
+        username: "admin",
+        password: "$2b$08$1E5vhqLG2zKzQMEPlH/2UuXwZK9j8bMYgCqtw6qCwc5XecbQ83RSq", // hash dla "1234"
+        permissions: "*"
+    }]
+},
